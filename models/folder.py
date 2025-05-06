6b9531fb8d3ea6e5a0ef1f4da86dbf717c85f8d1
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2025-05-06 17:58:40 +0200

    [ADD] added data file

--- models/folder.py
+++ models/folder.py
@@ -8,12 +8,11 @@ class Folder(models.Model):
     _name = 'document_hub.folder'
     _description = 'Document hub: Folder'
     
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string='Name', required=True, translation=True)
     active = fields.Boolean(string='Active', default=True)
     description = fields.Html(string='Description')
     sequence = fields.Integer('Sequence', default=10)
     company_id = fields.Many2one('res.company', string='Company', default=lambda lm: lm.env.company)
-
     parent_folder_id = fields.Many2one('document_hub.folder', string='Parent folder', ondelete='cascade')
     children_folder_ids = fields.One2many('document_hub.folder', 'parent_folder_id', string='Sub folder')
     document_ids = fields.One2many('document_hub.document', 'folder_id', string='Documents')
