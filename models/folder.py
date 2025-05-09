5c173ce4e36c8dcca1ee652fe1a3eb01c4fc88fb
Daniel Demedziuk <daniel.demedziuk@gmail.com>
2025-05-09 13:41:45 +0200

    [FIX] fixed security, many folders, documents and views

--- models/folder.py
+++ models/folder.py
@@ -13,37 +13,25 @@ class Folder(models.Model):
     active = fields.Boolean(string='Active', default=True)
     description = fields.Html(string='Description')
     sequence = fields.Integer('Sequence', default=10)
-    company_id = fields.Many2one(
-        'res.company', string='Company', default=lambda lm: lm.env.company)
-    parent_folder_id = fields.Many2one(
-        'document_hub.folder', string='Parent folder', ondelete='cascade')
-    children_folder_ids = fields.One2many(
-        'document_hub.folder', 'parent_folder_id', string='Sub folder')
-    document_ids = fields.One2many(
-        'document_hub.document', 'folder_id', string='Documents')
-    parent_path = fields.Char(
-        index=True, unaccent=False, compute='_compute_parent_path')
+    company_id = fields.Many2one('res.company', string='Company', default=lambda lm: lm.env.company)
+    parent_folder_id = fields.Many2one('document_hub.folder', string='Parent folder', ondelete='cascade')
+    children_folder_ids = fields.One2many('document_hub.folder', 'parent_folder_id', string='Sub folder')
+    document_ids = fields.One2many('document_hub.document', 'folder_id', string='Documents')
+    parent_path = fields.Char(index=True, unaccent=False, compute='_compute_parent_path', store=True)
 
-    visibility_administration = fields.Boolean(
-        string='Administration', default=False)
-    visibility_purchasing_and_logistics = fields.Boolean(
-        string='Purchasing and logistics', default=False)
+    visibility_administration = fields.Boolean(string='Administration', default=False)
+    visibility_purchasing_and_logistics = fields.Boolean(string='Purchasing and logistics', default=False)
     visibility_marketing = fields.Boolean(string='Marketing', default=False)
     visibility_accounting = fields.Boolean(string='Accounting', default=False)
     visibility_pm = fields.Boolean(string='PM', default=False)
     visibility_hr = fields.Boolean(string='HR', default=False)
-    is_project = fields.Boolean(string="Is project", default=False,
-                                help='Mark this option if you are sure this folder is for projects.')
+    is_project = fields.Boolean(string="Is project", default=False, help='Mark this option if you are sure this folder is for projects.')
 
     @api.depends('parent_folder_id', 'name')
     def _compute_parent_path(self):
-        print('COMPUTE PARENT PATH')
         for rec in self:
             if rec.parent_folder_id:
-                print('1')
-                rec.parent_path = f"{rec.parent_folder_id.name} > {rec.name}"
+                rec.parent_path = f"{rec.parent_folder_id.name}: {rec.name}"
             else:
-                print('2')
                 rec.parent_path = rec.name
 
-            print("PARENT PATH", rec.parent_path)
